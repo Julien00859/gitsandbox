59f8cafb0a2f4cb5f3c468049190a24c16b712a5
Julien Castiaux <j.castiaux@ephec.be>
2022-02-12 15:56:25 +0100

    feat(arithmetic): remove unused sub and div
    The sub and div operators have been added in the beginning of the
    project but it turns out the two are useless.
    
    Part of a global cleanup of our internal utils.

--- arithmetic.py
+++ arithmetic.py
@@ -6,21 +6,6 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-def sub(a: int, b: int) -> int:
-    """ Substract ``a`` by ``b``. """
-    return a + b
-
-
 def mul(a: int, b: int) -> int:
     """ Multiply two integers. """
     return a * b
-
-
-def div(a: int, b: int) -> int:
-    """
-    Divide ``a`` (dividend) by ``b`` (divisor) and return the quotient.
-
-    In case the dividend is not an integer multiple of the divisor,
-    the reminder is discarded and the integer quotient is returned.
-    """
-    return a // b
