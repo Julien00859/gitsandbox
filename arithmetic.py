c5427e6a882b3b447e4d81826dbbb5a09ae1f116
Julien Castiaux <j.castiaux@ephec.be>
2022-02-12 15:01:06 +0100

    feat(arithmetic): new sub, mul and div operators
    The arithmetic module lacked some important operators.

--- arithmetic.py
+++ arithmetic.py
@@ -4,3 +4,23 @@
 def add(a: int, b: int) -> int:
     """ Addition two integers. """
     return a + b
+
+
+def sub(a: int, b: int) -> int:
+    """ Substract ``a`` by ``b``. """
+    return a + b
+
+
+def mul(a: int, b: int) -> int:
+    """ Multiply two integers. """
+    return a * b
+
+
+def div(a: int, b: int) -> int:
+    """
+    Divide ``a`` (dividend) by ``b`` (divisor) and return the quotient.
+
+    In case the dividend is not an integer multiple of the divisor,
+    the reminder is discarded and the integer quotient is returned.
+    """
+    return a // b
